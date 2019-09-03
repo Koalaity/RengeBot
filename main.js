2061bb1e5871253c8f3e063542184d979588d2f3
Koalaity <47805044+Koalaity@users.noreply.github.com>
2019-09-03 19:43:21 -0400

    Test

---
+++ main.js
@@ -0,0 +1,16 @@
+/*
+ ____  ____  __ _   ___  ____ 
+(  _ \(  __)(  ( \ / __)(  __)
+ )   / ) _) /    /( (_ \ ) _) 
+(__\_)(____)\_)__) \___/(____)
+
+Written by Koalaity
+Licensed under GPL-3.0
+
+*/
+
+const { Client, Attachment } = require("discord.js");
+const client = new Client();
+const config = require("./config.json");
+
+
