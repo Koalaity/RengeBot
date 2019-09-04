7071d0077e92aa9939039e460e65a1c539072529
Koalaity <47805044+Koalaity@users.noreply.github.com>
2019-09-03 23:28:47 -0400

    Basic Command Handler (cmd + 1 arg)

--- main.js
+++ main.js
@@ -13,4 +13,38 @@ const { Client, Attachment } = require("discord.js");
 const client = new Client();
 const config = require("./config.json");
 
+function getCommand(msginput) {
+    let message = msginput.toString();
+    let parsedCommand = [];
+    if(message.includes("r!")) {
+        // Get Command
+        let command = message.slice(config.prefix.length).split(" ")[0];
+        parsedCommand.push(command);
 
+        //Get Argument
+        let end = message.indexOf(' ');
+        let args = message.substr(end, message.length);
+        parsedCommand.push(args);
+
+        return parsedCommand;
+    } else {
+        return false;
+    }
+}
+
+client.on('ready', () => {
+    console.log('RengeBot loaded!');
+});
+
+client.on('message', msg => {
+    var command = getCommand(msg);
+    //console.log(`${command[0]}, ${command[1]}`)
+    if(command[0] === "test") {
+        if(command[1].includes('gay')) {
+            msg.channel.send('gay')
+        }
+        msg.channel.send('works')
+    }
+});
+
+client.login(config.token);
